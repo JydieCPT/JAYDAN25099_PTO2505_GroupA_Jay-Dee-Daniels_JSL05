cf6c9c8b4e1bf9e6565a3a5d32ffaa22fade6db0
Jay-Dee Daniels <jydiecpt@gmail.com>
2025-07-25 19:02:29 +0200

    Add UID generator for task IDs.

--- scripts.js
+++ scripts.js
@@ -184,3 +184,11 @@ function handleDrop(e) {
     renderAll();
   }
 }
+
+// =========================
+// Utils
+// =========================
+
+function uid() {
+  return Math.random().toString(36).slice(2) + Date.now().toString(36);
+}
